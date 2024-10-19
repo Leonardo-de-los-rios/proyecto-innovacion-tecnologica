dcdb5b5eb7d36ed091b703d8ed23e5f1d7e1751b
Leonardo-de-los-rios <leodelosriosfacultad@gmail.com>
2024-10-19 01:45:24 -0300

    Gemini API: get started

--- gemini_api.py
+++ gemini_api.py
@@ -2,8 +2,8 @@ import os
 
 import google.generativeai as genai
 
-genai.configure(api_key=os.environ["API_KEY_GEMINI"])
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
 model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Write a story about a magic backpack.")
+response = model.generate_content("quien es el presidente actual de argentina?")
 print(response.text)
